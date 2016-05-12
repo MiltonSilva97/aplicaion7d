c7bb04aa2e015a44a88aa278dfd753437ff7b0ad
MiltonSilva97 <milton19994@gmail.com>
2016-05-12 13:43:13 -0500

    subida2

--- js/acciones.js
+++ js/acciones.js
@@ -2,11 +2,11 @@
 
 $(document).ready(function (e){
  document .addEventListener ("deviceready",function(){
-	 $('#btnbeep').tap(function(){
+	  $('#btnbeep').tap(function(){
 	     navigator.notification.beep(1);
       });//beep
  $('#btnvibrar').on('tap',function(){
 	 navigator.notification.vibrate(10000);
- });//vibrar
- },falce);
+ });
+ });
 });
